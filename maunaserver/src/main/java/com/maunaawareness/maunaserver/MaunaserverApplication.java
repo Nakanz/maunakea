234a48e5bb9f613be6ac30a9d1ce63b843989240
Nakanz <nathan.hester@gmail.com>
2019-08-28 12:00:57 -1000

    web and server stubs

---
+++ maunaserver/src/main/java/com/maunaawareness/maunaserver/MaunaserverApplication.java
@@ -0,0 +1,13 @@
+package com.maunaawareness.maunaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaunaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaunaserverApplication.class, args);
+	}
+
+}
