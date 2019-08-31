9dab5b6a1ba8fd2922a515d9d7fd1062fb85c59e
Nakanz <nathan.hester@gmail.com>
2019-08-30 17:34:32 -1000

    prepped UI for data retrieval

--- maunakeaweb/src/boot/axios.js
+++ maunakeaweb/src/boot/axios.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080', // will need to replace this with whatever base you are using
+  headers: { Pragma: 'no-cache' },
+});
+
 export default async ({ Vue }) => {
-  Vue.prototype.$axios = axios;
+  Vue.prototype.$axios = axiosInstance;
 };
+
+export { axiosInstance };
---
+++ maunakeaweb/src/services/data-service.js
@@ -0,0 +1,7 @@
+// import { axiosInstance } from 'src/boot/axios.js';
+// export default {
+//   async signUp() {
+//     return axiosInstance.post('/user/sign-up', user)
+//       .then(response => response);
+//   },
+// };
